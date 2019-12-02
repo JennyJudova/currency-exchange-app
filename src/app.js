d5c7498076f71ad44f97b3366fd55c7e5db97c5a
JennyJudova <jenny@Jennys-MacBook-Pro.local>
2019-12-02 18:47:15 +0000

    basic form and get request created

--- src/app.js
+++ src/app.js
@@ -12,74 +12,63 @@ class App extends React.Component {
 
     this.state = {
       data: {},
-      greeting: 'hello', 
-      currency: '', 
-      ammount: null,
+      greeting: 'hello',
+      currency: '',
+      amount: 0,
       date: ''
     }
 
     this.handleChange = this.handleChange.bind(this)
-    // this.onSelectedCurrency = this.onSelectedCurrency.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  componentDidMount() {
-    axios.get('https://api.exchangeratesapi.io/2000-01-01?base=USD') //`https://api.exchangeratesapi.io/${this.state.date}?base=${this.state.currency}`
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err))
+  handleSubmit(e) {
+    e.preventDefault()
+    axios.get(`https://api.exchangeratesapi.io/${this.state.date}?base=${this.state.currency}`)
+      .then(res => this.setState({ data: res.data }))
+      .catch(err => this.setState({ error: err.message }))
   }
-  
+
   handleChange(e) {
     console.log(e.target.name, e.target.value)
     this.setState({ [e.target.name]: e.target.value })
   }
 
-  // onSelectedCurrency(e) {
-  //   console.log(e.target.value)
-  // }
-
-
   render() {
-    console.log(this.state.currency, this.state.ammount, this.state.date)
+    console.log(this.state.data)
+    console.log(this.state.currency, this.state.amount, this.state.date)
     return (
-      <>
-      <h1>{this.state.greeting}</h1>
-      <p>How much would you recieve for CURRENCY AMMOUNT in DATE</p>
-      <form>
-        <div className='currency'>
-          <div>
-            <SelectCurrency 
-              type='currency'
-              name='currency'
-              onChange={this.handleChange}
-              value={this.state.currency}
-            />
-          </div>
-          <input
-            type='date'
-            name='date'
+      <form onSubmit={this.handleSubmit}>
+        <h1>{this.state.greeting}</h1>
+        <p>How much would you recieve for CURRENCY AMOUNT in DATE</p>
+        <div>
+          <SelectCurrency
+            type='currency'
+            name='currency'
             onChange={this.handleChange}
-            value={this.state.date}
+            value={this.state.currency}
           />
         </div>
-
+        <input
+          type='date'
+          name='date'
+          onChange={this.handleChange}
+          value={this.state.date}
+        />
+        <input
+          type='amount'
+          name='amount'
+          onChange={this.handleChange}
+          value={this.state.amount}
+        />
+        <button type='submit'>Submit</button>
       </form>
-      </>
     )
   }
 }
-  
- 
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
-
-
-
-
-{/* <input
-type='currency'
-name='currency'
-onChange={this.handleChange}
-value={this.state.currency}
-/> */}
